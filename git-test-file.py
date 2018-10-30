d4ee1f738bf0c5c6d8798a387bcff684b8ee61cd
kayanpang <kayanpang@gmail.com>
2018-10-30 11:43:11 -0400

    testing one 181030 class one

--- git-test-file.py
+++ git-test-file.py
@@ -1,8 +1,8 @@
 fname = "Pang"
 name = "KaYan"
-number = "17"
+number = 17
 
-result = fname.upper() + " " + name.upper () + "; and my favorite number" \
+result = fname.upper() + " " + name.upper() + "; and my favorite number" \
          + " number is " + str(number)
 
 print(result)
