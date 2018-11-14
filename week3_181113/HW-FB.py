82e2c93caa26716d642c69f407988216f10cdcf9
kayanpang <kayanpang@gmail.com>
2018-11-14 17:51:36 -0500

    week3 homework collaboration 181114 and elif end

--- week3_181113/HW-FB.py
+++ week3_181113/HW-FB.py
@@ -1,9 +1,9 @@
 for a in range(1,25):
 
     if a % 3 == 0:
-        print("fizz", end=" ")
+        print(str(a) + " fizz", end=" ")
     if a % 5 == 0:
-        print("buzz", end=" ")
+        print(str(a) + " buzz", end=" ")
     if a % 3 == 0 and a % 5 == 0:
-        print("fizzbuzz", end=" ")
+        print(str(a) + " fizzbuzz", end=" ")
     print(str(a))
