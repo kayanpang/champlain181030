f80a0ddfd69dbd29f3f865e53334d671d7e9b33b
kayanpang@gmail.com <Shonbok3>
2019-01-07 01:35:06 -0500

    190107 try group project

---
+++ group-project01/basic-order-system.py
@@ -0,0 +1,11 @@
+class Item:
+    def __init__(self, name, price, taxable):
+        self.sku = sku
+        self.name = name
+        self.price = price
+        self.taxable = taxable
+
+    def print_item(self):
+        line =
+        for x in line:
+            
