7adbf081a7393e02a0c3056aeb51722688b2dda2
kayanpang <kayanpang@gmail.com>
2019-01-15 13:03:24 -0500

    190115 cont' file, exercises on slides, and csv. quiz on Class, File, and csv. update

--- week-10_file/directory_exercise1.py
+++ week-10_file/directory_exercise1.py
@@ -5,6 +5,11 @@ import os
 #    os.remove("d")
 #    if IOError:
 #        return os.mkdir("d")
+# why return doesn't work: possible explanation
+# That’s exactly what return is supposed to do. If at some point in a method you say return,
+# it’s like saying "stop whatever it is you’re doing and get outta here NOW",
+# so anything after the return keyword is ignored.
+# https://www.codecademy.com/en/forum_questions/51c0e35d7c82caace80008b1
 
 dirname = "TestDir"
 # create function
