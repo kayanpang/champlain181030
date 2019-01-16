80bdba8fa64ff1d2718877e4b4d1e370af8f94fd
kayanpang <kayanpang@gmail.com>
2019-01-16 18:00:44 -0500

    190116 revision

---
+++ review_quiz-class-file-csv/Class review.py
@@ -0,0 +1,8 @@
+class Dog:
+# create a class
+
+class Dog():
+# create object (use class to create object) every object is built from a class
+# it's not necessary to use () as in crash course. If there's nothing in the (), then there's no difference
+# inheritance
+
