4d58e33b4ee69c9d4f65de5eab27d584ce12d4b9
kayanpang <kayanpang@gmail.com>
2019-01-29 16:39:48 -0500

    190129 CSV class assignment + quiz review update

--- week11-2_assignment-csv/class-structure.py
+++ week11-2_assignment-csv/class-structure.py
@@ -1,12 +1,29 @@
 class Document:
-    def __init__(self, author=[], date):
+    def __init__(self, author = [], date):
         self.__author = author
         self.__date = datetime.datetime.now()
     def getAuthor(self):
+        return self.__author
+
+    def addAuthor(self, name):
+        self.__author.append(name)
+
+    def getDate(self):
+        return self.__date
 
 class Email(Document):
-    def __init__(self, subject, to=[]):
+    def __init__(self, subject, to = []):
         self.__subject = subject
         self.__to = to
 
+    def getSubject(self):
+        return self.__subject
+    def getTo(self):
+        return.__to
+
 class Book(Document):
+    def __init__(self, title = ""):
+        self.__title = title
+
+    def getTitle(self):
+        return self.__title
