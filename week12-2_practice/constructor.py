993f5dc26ede94420b03b391ee569447fc433a7b
kayanpang <kayanpang@gmail.com>
2019-02-05 10:54:05 -0500

    190205 cont' practice project on class

--- week12-2_practice/constructor.py
+++ week12-2_practice/constructor.py
@@ -1,7 +1,11 @@
 import datetime
 class Worker:
-    def __init__(self, id: int, first_name: str, last_name: str, hire_date: datetime):
-        self.__id = id
+    """This represents"""
+    auto_id = 0
+    def __init__(self, first_name: str, last_name: str, hire_date: datetime):
+        self.__id = Worker.auto_id + 1
+        Worker.auto_id = self.__id
+        # this will give id by program
         self.__first_name = first_name
         self.__last_name = last_name
         self.__hire_date = hire_date
@@ -21,31 +25,31 @@ class Worker:
 
 
 class Employee(Worker):
-    def __init__(self, id: int, first_name: str, last_name: str, hire_date: datetime, salary: float):
+    def __init__(self, first_name: str, last_name: str, hire_date: datetime, salary: float):
         self.__salary = salary
-        super().__init__(id, first_name, last_name, hire_date)
+        super().__init__(first_name, last_name, hire_date)
     def get_salary(self, salary: float):
         self.__salary = salary
-    def set_salary(self):
+    def get_salary(self):
         return self.__salary
     def get_hourly_rate(self):
         return self.__salary / 261 / 8
 
 
 class Contractor(Worker):
-    def __init__(self, id: int, first_name: str, last_name: str, hire_date: datetime, hourly_rate: float):
+    def __init__(self, first_name: str, last_name: str, hire_date: datetime, hourly_rate: float):
         self.__hourly_rate = hourly_rate
-        super().__init__(id, first_name, last_name, hire_date)
-    def get_hourly_rate(self, hourly_rate: float):
+        super().__init__(first_name, last_name, hire_date)
+    def set_hourly_rate(self, hourly_rate: float):
         self.__hourly_rate = hourly_rate
-    def set_hourly_rate(self):
+    def get_hourly_rate(self):
         return self.__hourly_rate
 
 class Task:
-    def __init__(self, id: int, name: str, description: str, assign_to: Worker, duration_hour: float):
+    def __init__(self, name: str, description: str, assign_to: Worker, duration_hour: float):
         self.__id = id
         self.__name = name
-        self. __description = description
+        self.__description = description
         self.__assign_to = assign_to
         self.__duration_hour = duration_hour
         self.__hours_completed = 0.0
@@ -70,10 +74,10 @@ class Task:
         return self.__assign_to
     def get_duration_hour(self):
         return self.__duration_hour
-    def hours_completed(self):
+    def get_hours_completed(self):
         return self.__hours_completed
     def add_time(self, num_hours: float):
-        self.__hours_complete += num_hours
+        self.__hours_completed += num_hours
 
 class Project:
     def __init__(self, id: int, name: str, hour_estimated: float):
@@ -89,10 +93,34 @@ class Project:
 
     def get_id(self):
         return self.__id
-    def get__name(self):
+    def get_name(self):
         return self.__name
     def get_hour_estimated(self):
         return self.__hour_estimated
     def add_task(self, task: Task):
         # has to specify task as object, not a string
         self.__tasks.append(task)
+    def get_project_cost(self):
+        total_cost = 0.00
+        for task in self.__tasks:
+            total_cost += task.get_duration_hour() * task.get_assigned_to().get_hourly_rate()
+        return total_cost
+    def get_project_elapsed_cost(self):
+        total_elapsed_cost = 0.00
+        for task in self.__tasks:
+            total_elapsed_cost += task.get_hours_completed() * task.get_assigned_to().get_hourly_rate()
+        return total_elapsed_cost
+
+emp = Employee("Brendan", "Wood", None, 75000)
+contr = Contractor("Alice", "Smith", None, 78)
+
+task1 = Task(1, "Cook breakfast", "Cook a great breakfast", emp, 10)
+task2 = Task(2, "Make Lunch", "Lunch is important", contr, 10)
+task1.add_time(8)
+task2.add_time(9)
+proj1 = Project(1, "Project #1", 30)
+
+proj1.add_task(task1)
+proj1.add_task(task2)
+print("The total estimated cost for project " + proj1.get_name() + " is " + '$(:,.2f'.format(proj1.get_project_cost()))
+print("The final cost for project " + proj1.get_name() + " is " + '$(:,.2f'.format(proj1.get_project_elapsed_cost()))
