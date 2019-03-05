9af6dc5c87023f5d7500610a52edd814e7d16578
kayanpang@gmail.com <Shonbok3>
2019-03-05 01:20:08 -0500

    190304 add note to revision

--- 190225_revision/different-kinds-of-array.py
+++ 190225_revision/different-kinds-of-array.py
@@ -1,20 +1,29 @@
 # list is ordered and changeable. Allows duplicate members. it uses [] square brackets
+# – Use lists if you have a collection of data that does not need random access.
+# Try to choose lists when you need a simple, iterable collection that is modified frequently.
 mylist = [1, 2, 'Brendan']
 print(mylist[1])
 
 # tuple is ordered and unchangeable. Allows duplicate members. it uses () round brackets
+# – Use tuples when your data cannot change.
 mytuple = ('apple', 'banana', 'cherry', 1)
 print(mytuple[1])
 
 
 # a set is unordered and unindexed. it uses {} curly bracket
 # need a for loop to get access to the set, cannot refer to index
+# – Use a set if you need uniqueness for the elements
 myset = {'apple', 'banana', 'cherry', 1, 2}
 for x in myset:
     print(x)
 
 # a dictionary is unordered, changeable and indexed. it has keys and values. it uses {} curly bracket
 # can access the items in a dictionary using its key name
+# When to use a dictionary:
+# – When you need a logical association between a key:value pair.
+# – When you need fast lookup for your data, based on a custom key.
+# – When your data is being constantly modified. Remember, dictionaries are mutable.
+# https://monjurulhabib.wordpress.com/2016/09/22/python-when-to-use-list-vs-tuple-vs-dictionary-vs-set-theory/
 mydict = {
     "brand": "Ford",
     "model": "Mustang",
